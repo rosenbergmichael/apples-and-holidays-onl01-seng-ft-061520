21f7ac63ca614cbc27ec5807949607d61ef1152e
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-22 17:53:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
   holiday_hash[:fall][:holiday] = "supply"
-  puts holiday_hash
+  return holiday_hash
 end
 
 
