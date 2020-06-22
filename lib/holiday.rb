603c72f0751c152a4775e3a86b0b49bd76c0abac
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-22 17:59:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,6 +58,9 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter]
 end
 
+
+
+
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
