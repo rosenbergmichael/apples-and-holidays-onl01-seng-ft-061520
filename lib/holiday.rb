6b3d8468de3d5a263e81569078fc9763edfb2e24
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-22 18:25:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |holiday, supply|
+holiday_hash.each do |season, holiday|
  binding.pry
   end
 end 
